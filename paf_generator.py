0a094bb633932078d4d50cf337230447b0620573
Sean <sel118@ucsd.edu>
2020-11-22 16:12:08 -0800

    paf_generator.py modified

--- paf_generator.py
+++ paf_generator.py
@@ -16,6 +16,5 @@ def generate_pafs():
     for i in image_filenames:
         label = cv2.imread(i)
         split_name = i.split('/')
-        print(i)
         generatedPAFs = paf.generatePAFs(label[:, :, 0], viz=False)
         np.save(os.path.join(split_name[0], split_name[1], split_name[2], split_name[3], split_name[4], '20.npy'), generatedPAFs)
