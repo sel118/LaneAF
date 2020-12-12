d79cfe670180aacd2d5b6d64955d18140e35cfc5
Sean <sel118@ucsd.edu>
2020-12-11 16:19:45 -0800

    testbranch commit 2

--- visualize.py
+++ visualize.py
@@ -19,6 +19,21 @@ def tensor2image(tensor, mean, std):
     image = image[:, :, ::-1] # RGB to BGR
     return image.astype(np.uint8) # (H, W, C)
 
-def create_viz(img, mask, vaf, haf):
+def create_viz(img, mask, VAF, HAF):
     im_out = [] #test
+    print(img.shape)
+    print(mask.shape)
+    print(VAF.shape)
+    print(HAF.shape)
+    jdasifji
+    #down_rate = 5 # downsample visualization by this factor
+    #fig, (ax1, ax2, ax3, ax4) = plt.subplots(2, 2)
+    
+    # visualize VAF
+    #q = ax3.quiver(np.arange(0, label.shape[1], down_rate), -np.arange(0, label.shape[0], down_rate), 
+    #               VAF[::down_rate, ::down_rate, 0], -VAF[::down_rate, ::down_rate, 1], scale=120)
+    # visualize HAF
+    #q = ax4.quiver(np.arange(0, label.shape[1], down_rate), -np.arange(0, label.shape[0], down_rate), 
+    #               HAF[::down_rate, ::down_rate, 0], -HAF[::down_rate, ::down_rate, 1], scale=120)
+    #plt.show()
     return im_out
